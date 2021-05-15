a446ccf6c2018b89e32a014f54f5fbc67f52d262
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-05-15 11:29:44 +0700

    Cool

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld />
+  <div
+    class="drop-zone"
+    @drop="onDrop($event, 1)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div
+      v-for="item in getList(1)"
+      :key="item.id"
+      class="drag-el"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
+
+  <div
+    class="drop-zone"
+    @drop="onDrop($event, 2)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
+    <div
+      v-for="item in getList(2)"
+      :key="item.id"
+      class="drag-el"
+      draggable="true"
+      @dragstart="startDrag($event, item)"
+    >
+      {{ item.title }}
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-// import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+<script lang="ts">
+import { defineComponent, ref } from "vue";
 
-// export default defineComponent({
-//   name: "App",
-//   components: {
-//     HelloWorld,
-//   },
-// });
+export default defineComponent({
+  name: "App",
+  setup() {
+    const items = ref([
+      {
+        id: 0,
+        title: "Item A",
+        list: 1,
+      },
+      {
+        id: 1,
+        title: "Item B",
+        list: 1,
+      },
+      {
+        id: 2,
+        title: "Item C",
+        list: 2,
+      },
+    ]);
+
+    const getList = (list: any) =>
+      items.value.filter((item) => item.list === list);
+
+    const startDrag = (evt: any, item: any) => {
+      evt.dataTransfer.dropEffect = "move";
+      evt.dataTransfer.effectAllowed = "move";
+      evt.dataTransfer.setData("itemID", item.id);
+    };
+
+    const onDrop = (evt: any, list: any) => {
+      const itemID = evt.dataTransfer.getData("itemID");
+      const item = items.value.find((item) => item.id == itemID);
+      if (item) {
+        item.list = list;
+      }
+    };
+
+    return { getList, startDrag, onDrop };
+  },
+});
 </script>
 
 <style>
@@ -23,5 +88,17 @@ import HelloWorld from "./components/HelloWorld.vue";
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.drop-zone {
+  background-color: #eee;
+  margin-bottom: 10px;
+  padding: 10px;
+}
+
+.drag-el {
+  background-color: #fff;
+  margin-bottom: 10px;
+  padding: 5px;
 }
 </style>
